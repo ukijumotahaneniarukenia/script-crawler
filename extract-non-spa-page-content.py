76dfefa3cffac348b952515f3ba4b492c11c1534
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-19 18:08:46 +0900

    sketch

--- extract-non-spa-page-content.py
+++ extract-non-spa-page-content.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 import glob
 
@@ -16,6 +17,9 @@ from lxml import html
 
 SOURCE_FILE_NAME='list-non-spa.json'
 
+LINK_PREFFIX='link-'
+LINK_SUFFIX='.txt'
+
 INPUT_PREFIX = 'page-detail-'
 INPUT_SUFFIX = '.html'
 OUTPUT_PREFIX = 'page-content-'
@@ -32,17 +36,6 @@ SITE_URL='SITE_URL'
 SUB_XPATH_EXPRESSION='SUB_XPATH_EXPRESSION'
 TITLE_NAME='TITLE_NAME'
 
-#取得項目値の格納用の変数
-EXTRACT_BASE_NAME=''
-EXTRACT_PAGE_NAME=''
-EXTRACT_SITE_NAME=''
-EXTRACT_SITE_URL=''
-
-EXTRACT_TITLE_NAME='-'
-EXTRACT_DATE_TIME='-'
-EXTRACT_IS_COMPLETED_TITLE_NAME_FLG='0'
-EXTRACT_IS_COMPLETED_DATE_TIME_FLG='0'
-
 DTM = datetime.datetime.today().strftime('%Y-%m-%dT%H-%M-%S')
 DST = os.getcwd() #実行ディレクトリ
 
@@ -50,103 +43,139 @@ source_file = open(SOURCE_FILE_NAME,'r')
 
 crawler_target_list = json.load(source_file)
 
-input_file_name_list = glob.glob(INPUT_PREFIX + '*' + INPUT_SUFFIX)
+for crawler_target in crawler_target_list:
 
-for input_file_name in input_file_name_list:
+    site_name = crawler_target[SITE_NAME]
+    site_url = crawler_target[SITE_URL]
+    base_name = "-".join(re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0].split(".")[::-1])
 
-    extract_list = []
+    link_file_name = LINK_PREFFIX + base_name + LINK_SUFFIX
 
-    EXTRACT_PAGE_NAME = input_file_name
-    extract_list.append(EXTRACT_PAGE_NAME)
+    if os.path.exists(link_file_name):
 
-    for crawler_target in crawler_target_list:
+        if re.search(base_name,link_file_name):
 
-        site_name = crawler_target[SITE_NAME]
-        site_url = crawler_target[SITE_URL]
-        base_name = "-".join(re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0].split(".")[::-1])
+            link_file_name_list = open(link_file_name,'r')
 
-        if re.search(base_name,input_file_name):
+            for link_file_name in link_file_name_list:
 
-            EXTRACT_SITE_NAME = crawler_target[SITE_NAME]
-            EXTRACT_SITE_URL = crawler_target[SITE_URL]
-            EXTRACT_BASE_NAME = base_name
+               domain_name = re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0]
 
-            extract_list.append(EXTRACT_SITE_NAME)
-            extract_list.append(EXTRACT_SITE_URL)
-            extract_list.append(EXTRACT_BASE_NAME)
+               input_file_name = INPUT_PREFIX + link_file_name.strip().replace(domain_name,base_name).replace('/','-').replace('&','-').replace('?','-').replace('^','-').replace(':','-').replace('#','-') + INPUT_SUFFIX
 
-            target_file = open(input_file_name,'r')
+               if os.path.exists(input_file_name):
 
-            target_file_content = target_file.read()
+                   extract_list = []
 
-            target_dom = html.fromstring(target_file_content)
+                   if re.search(base_name,input_file_name) and ( not site_url ==link_file_name.strip() + "/" ):
+                       #トップエントリ以外を処理対象とする
 
-            target_xpath_list = crawler_target[TITLE_NAME]
+                       #取得項目値の格納用の変数
+                       EXTRACT_TITLE_NAME='-'
+                       EXTRACT_DATE_TIME='-'
+                       EXTRACT_IS_COMPLETED_TITLE_NAME_FLG='0'
+                       EXTRACT_IS_COMPLETED_DATE_TIME_FLG='0'
 
-            for target_xpath in target_xpath_list:
+                       EXTRACT_URL_NAME = link_file_name.strip()
+                       extract_list.append(EXTRACT_URL_NAME)
 
-                main_xpath = target_xpath[MAIN_XPATH_EXPRESSION]
-                sub_xpath = target_xpath[SUB_XPATH_EXPRESSION]
+                       EXTRACT_PAGE_NAME = input_file_name
+                       extract_list.append(EXTRACT_PAGE_NAME)
 
-                if not len(main_xpath) == 0 :
+                       EXTRACT_SITE_NAME = crawler_target[SITE_NAME]
+                       EXTRACT_SITE_URL = crawler_target[SITE_URL]
+                       EXTRACT_BASE_NAME = base_name
 
-                    result_list = target_dom.xpath(main_xpath)
+                       extract_list.append(EXTRACT_SITE_NAME)
+                       extract_list.append(EXTRACT_SITE_URL)
+                       extract_list.append(EXTRACT_BASE_NAME)
 
-                    if not len(result_list) == 0:
+                       target_file = open(input_file_name,'r')
 
-                        extract_title_name = result_list[0].text
+                       target_file_content = target_file.read()
 
-                        if extract_title_name is not None :
+                       target_dom = html.fromstring(target_file_content)
 
-                            EXTRACT_TITLE_NAME = extract_title_name
-                            EXTRACT_IS_COMPLETED_TITLE_NAME_FLG = '1'
+                       target_xpath_list = crawler_target[TITLE_NAME]
 
-                            extract_list.append(EXTRACT_TITLE_NAME)
-                            extract_list.append(EXTRACT_IS_COMPLETED_TITLE_NAME_FLG)
+                       for target_xpath in target_xpath_list:
 
-                if not len(sub_xpath) == 0 :
-                    #複数件への対応
+                           main_xpath = target_xpath[MAIN_XPATH_EXPRESSION]
+                           sub_xpath = target_xpath[SUB_XPATH_EXPRESSION]
 
-                    pass
+                           if not len(main_xpath) == 0 :
 
-            target_xpath_list = crawler_target[DATE_TIME]
+                               result_list = target_dom.xpath(main_xpath)
 
-            for target_xpath in target_xpath_list:
+                               if not len(result_list) == 0:
 
-                main_xpath = target_xpath[MAIN_XPATH_EXPRESSION]
-                sub_xpath = target_xpath[SUB_XPATH_EXPRESSION]
+                                   extract_title_name = result_list[0].text
 
-                if not len(main_xpath) == 0 :
+                                   if extract_title_name is not None :
 
-                    result_list = target_dom.xpath(main_xpath)
+                                       EXTRACT_TITLE_NAME = extract_title_name
+                                       EXTRACT_IS_COMPLETED_TITLE_NAME_FLG = '1'
 
-                    if not len(result_list) == 0:
+                                       extract_list.append(EXTRACT_TITLE_NAME)
+                                       extract_list.append(EXTRACT_IS_COMPLETED_TITLE_NAME_FLG)
 
-                        extract_date_time = result_list[0].text
+                                   else :
 
-                        if extract_date_time is not None :
+                                       extract_list.append(EXTRACT_TITLE_NAME)
+                                       extract_list.append(EXTRACT_IS_COMPLETED_TITLE_NAME_FLG)
 
-                            EXTRACT_DATE_TIME = extract_date_time
-                            EXTRACT_IS_COMPLETED_DATE_TIME_FLG = '1'
+                           if not len(sub_xpath) == 0 :
+                               #複数件への対応
 
-                            extract_list.append(EXTRACT_DATE_TIME)
-                            extract_list.append(EXTRACT_IS_COMPLETED_DATE_TIME_FLG)
+                               pass
 
-                if not len(sub_xpath) == 0 :
-                    #複数件への対応
+                       target_xpath_list = crawler_target[DATE_TIME]
 
-                    pass
+                       for target_xpath in target_xpath_list:
 
-            target_file.close()
+                           main_xpath = target_xpath[MAIN_XPATH_EXPRESSION]
+                           sub_xpath = target_xpath[SUB_XPATH_EXPRESSION]
 
-    #前回分の出力結果ファイルが存在すれば削除
-    #if os.path.exists(output_file_name):
+                           if not len(main_xpath) == 0 :
 
-    #    os.remove(output_file_name)
+                               result_list = target_dom.xpath(main_xpath)
 
-    #with open(output_file_name,'a') as f:
+                               if not len(result_list) == 0:
 
-    #    f.write(response.text)
-    #    f.write(ORS)
+                                   extract_date_time = result_list[0].text
 
-    print("\t".join(extract_list))
+                                   if extract_date_time is not None :
+
+                                       EXTRACT_DATE_TIME = extract_date_time
+                                       EXTRACT_IS_COMPLETED_DATE_TIME_FLG = '1'
+
+                                       extract_list.append(EXTRACT_DATE_TIME)
+                                       extract_list.append(EXTRACT_IS_COMPLETED_DATE_TIME_FLG)
+
+                                   else :
+
+                                       extract_list.append(EXTRACT_DATE_TIME)
+                                       extract_list.append(EXTRACT_IS_COMPLETED_DATE_TIME_FLG)
+
+                           if not len(sub_xpath) == 0 :
+                               #複数件への対応
+
+                               pass
+
+                       target_file.close()
+
+                   else:
+
+                       continue
+
+                   #前回分の出力結果ファイルが存在すれば削除
+                   #if os.path.exists(output_file_name):
+
+                   #    os.remove(output_file_name)
+
+                   #with open(output_file_name,'a') as f:
+
+                   #    f.write(response.text)
+                   #    f.write(ORS)
+
+                   print("\t".join(extract_list))
