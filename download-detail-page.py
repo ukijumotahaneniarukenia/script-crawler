341e3ff6efd6a27575878adf7b41a1391b9818a6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-26 01:37:14 +0900

    sketch

--- download-detail-page.py
+++ download-detail-page.py
@@ -3,59 +3,93 @@ import os
 
 import re
 
+import json
+
 import requests
 
-SOURCE_FILE_NAME='base-file-name-list.txt'
+import time
 
-INPUT_PREFIX = 'link-'
-INPUT_SUFFIX = '.txt.min'
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from selenium.common.exceptions import NoSuchElementException
+
+DEFAULT_WAIT_TIME_SECONDS=5
+
+SOURCE_FILE_NAME='list.json'
+
 OUTPUT_PREFIX = 'page-detail-'
 OUTPUT_SUFFIX = '.html'
 
+OFS = '\t'
 ORS = '\n'
+
+#取得項目の定義
+SITE_NAME='SITE_NAME'
+SITE_URL='SITE_URL'
+MIN_PAGE_NUMBER='MIN_PAGE_NUMBER'
+MAX_PAGE_NUMBER='MAX_PAGE_NUMBER'
 
 source_file = open(SOURCE_FILE_NAME,'r')
 
-for site_url in source_file :
+crawler_target_list = json.load(source_file)
 
+#ブラウザ起動オプションの設定
+options = webdriver.ChromeOptions()
+options.add_argument('/usr/local/src/chromedriver_linux64/chromedriver')
+options.add_argument('/usr/local/src/chrome-linux/chrome')
+
+for crawler_target in crawler_target_list:
+
+    site_name = crawler_target[SITE_NAME]
+    site_url = crawler_target[SITE_URL]
+    min_page_number = crawler_target[MIN_PAGE_NUMBER]
+    max_page_number = crawler_target[MAX_PAGE_NUMBER]
     base_name = "-".join(re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0].split(".")[::-1])
 
-    input_file_name = INPUT_PREFIX + base_name + INPUT_SUFFIX
+    output_file_name = OUTPUT_PREFIX + base_name + OUTPUT_SUFFIX
 
-    if os.path.exists(input_file_name):
+    #前回分の出力結果ファイルが存在すれば削除
+    if os.path.exists(output_file_name):
 
-        target_file = open(input_file_name,'r')
+        os.remove(output_file_name)
 
-        for url in target_file:
+    for page_number in range(min_page_number,max_page_number+1):
 
-            response = requests.get(url.strip())
+        crawler_target_url = site_url + str(page_number)
 
-            status_code = response.status_code
+        print(crawler_target_url)
 
-            if status_code == 200:
+        response = requests.get(crawler_target_url.strip())
 
-                domain_name = re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0]
+        status_code = response.status_code
 
-                output_file_name = OUTPUT_PREFIX + url.strip().replace(domain_name,base_name).replace('/','-').replace('&','-').replace('?','-').replace('^','-').replace(':','-').replace('#','-').replace('=','-') + OUTPUT_SUFFIX
+        if status_code == 200:
 
-                #前回分の出力結果ファイルが存在すれば削除
-                if os.path.exists(output_file_name):
+            driver = webdriver.Chrome(options=options)
 
-                    os.remove(output_file_name)
+            driver.get(crawler_target_url)
 
-                with open(output_file_name,'a') as f:
+            time.sleep(DEFAULT_WAIT_TIME_SECONDS)
 
-                    f.write(response.text)
-                    f.write(ORS)
+            html = driver.page_source
 
-            else:
+            domain_name = re.findall(r'(?<=//).*?(?=/)', site_url.strip())[0]
 
-                continue
+            output_file_name = OUTPUT_PREFIX + crawler_target_url.strip().replace(domain_name,base_name).replace('/','-').replace('&','-').replace('?','-').replace('^','-').replace(':','-').replace('#','-').replace('=','-') + OUTPUT_SUFFIX
 
-        target_file.close()
+            #前回分の出力結果ファイルが存在すれば削除
+            if os.path.exists(output_file_name):
 
-    else:
+                os.remove(output_file_name)
 
-        continue
+            with open(output_file_name, 'w', encoding='utf-8') as f:
+                f.write(html)
+
+            driver.quit()
+
+        else:
+
+            continue
 
 source_file.close()
